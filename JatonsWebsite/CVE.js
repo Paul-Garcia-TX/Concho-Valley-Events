767f46a68a46a5014a7603f5752bddc29b26691c
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-27 22:10:16 -0500

    weather info created for website, moving on to creating json

---
+++ JatonsWebsite/CVE.js
@@ -0,0 +1,36 @@
+// get weather data
+let zipCode = 76904;
+function fetchWeatherData(zipCode){
+    $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/weather",
+        type: "GET",
+        data: {
+            APPID: openWeather,
+            zip: zipCode,
+            units: "imperial",
+        }
+    }).done(function(data){
+        console.log(data);
+        let html = "";
+
+        const currentDate = new Date(data.dt * 1000).toLocaleDateString();
+
+        html += '<h6>Todays Weather: ' + currentDate + '</h6>';
+        html += '<div>';
+        html += '<img src="https://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
+        html += '</div>';
+        html += '<p >Conditions: ' + data.weather[0].description + ' </p>';
+        html += '<p>Temp: ' + parseInt(data.main.temp) + '&#8457; </p>';
+        html += '<p>High: ' + parseInt(data.main.temp_max) + '&#8457; </p>';
+        html += '<p>Low: ' + parseInt(data.main.temp_min) + '&#8457; </p>';
+
+        // html += '<p>Humidity: ' + data.humidity + '% </p>';
+
+
+        document.getElementById("weatherInfo").innerHTML = html;
+
+    })
+}
+
+fetchWeatherData(zipCode);
+
