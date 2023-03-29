48e2ae0069ffef48ef32c869fa28b7f4c2629a5f
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-29 11:01:04 -0500

    cards been rendered, need styling

--- JatonsWebsite/CVE.js
+++ JatonsWebsite/CVE.js
@@ -35,14 +35,54 @@ function fetchWeatherData(zipCode){
 fetchWeatherData(zipCode);
 
 $.get('events.json').done(function (data){
+    console.log(data);
+    renderEvents(data);
+})
+    .catch(function(error){
+        console.error(error);
+    });
+
+function renderEvent(event, id){
     let eventInfo = "";
 
-    const firstData= data[0];
+    eventInfo += '<div class="col" id="' + id + '">';
+    eventInfo += '<div class="card">';
+    eventInfo += '<div class="row g-0">';
+    eventInfo += '<div class="col-md-4">';
+    eventInfo += '<img src="' + event.image + '" class="img-fluid rounded-start" alt="eventImage"> ';
+    eventInfo += '</div>';
+    eventInfo += '<div class="col-md-8">';
+    eventInfo += '<div class="card-body">'
+    eventInfo += '<h4>' + event.title + '</h4>';
+    eventInfo += '<h6> When: ' + event.date + '</h6>';
+    eventInfo += '<p> Where: <a href="' + event.location + '" target="_blank"><i class="fa-solid fa-map-location-dot"></i></a></p>';
+    eventInfo += '<p>' + event.desc + '</p>';
+    eventInfo += '<div id="icons area">';
+    eventInfo += '<a href="'+ event.facebook + '" target="_blank">';
+    eventInfo += '<i class="fa-brands fa-facebook" style="margin: 1em;"></i>';
+    eventInfo += '</a>';
+    eventInfo += '<a href="'+ event.instagram + '" target="_blank">';
+    eventInfo += '<i class="fa-brands fa-instagram" style="margin: 1em;"></i>';
+    eventInfo += '</a>';
+    eventInfo += '<a href="mailto:'+ event.email + '" target="_blank">';
+    eventInfo += '<i class="fa-solid fa-envelope" style="margin: 1em;"></i>';
+    eventInfo += '</a>';
+    eventInfo += '<a href="'+ event.website + '" target="_blank">';
+    eventInfo += '<i class="fa-solid fa-info" style="margin: 1em;"></i>';
+    eventInfo += '</a>';
+    eventInfo += '</div>';
+    eventInfo += '</div>';
+    eventInfo += '</div>';
+    eventInfo += '</div>';
 
-    eventInfo += '<h4>' + firstData.title + '</h4>';
-    eventInfo += '<h6>' + firstData.date + '</h6>';
-    eventInfo += '<p>' + firstData.desc + '</p>';
+    return eventInfo;
+}
 
-
-    $("#firstCard").append(eventInfo);
-})
+function renderEvents(events){
+    let eventInfo = '';
+    for(let i = events.length -1; i>=0; i--){
+        let id = 'event-' + i;
+        eventInfo += renderEvent(events[i], id);
+    }
+    document.getElementById('cardsRow').innerHTML = eventInfo;
+}
