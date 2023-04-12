9a167af9fab0ea86767edf700029f2b0ace6212f
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-12 16:18:38 -0500

    good progress need to start moving towards mobile friendly

--- JatonsWebsite/CVE.js
+++ JatonsWebsite/CVE.js
@@ -16,15 +16,13 @@ function fetchWeatherData(zipCode){
         const currentDate = new Date(data.dt * 1000).toLocaleDateString();
         html += '<div id="weatherInfoBody" class="container">';
         html += '<p>Todays Weather: ' + currentDate + '</p>';
-        // // html += '<div>';
-        // html += '<img src="https://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
-        // // html += '</div>';
+
         html += '<p >Conditions: ' + data.weather[0].description + ' </p>';
         html += '<p>Temp: ' + parseInt(data.main.temp) + '&#8457; </p>';
         html += '<p>High: ' + parseInt(data.main.temp_max) + '&#8457; </p>';
         html += '<p>Low: ' + parseInt(data.main.temp_min) + '&#8457; </p>';
         html += '</div>';
-        // html += '<p>Humidity: ' + data.humidity + '% </p>';
+
 
 
         document.getElementById("weatherInfo").innerHTML = html;
@@ -35,7 +33,7 @@ function fetchWeatherData(zipCode){
 fetchWeatherData(zipCode);
 
 $.get('events.json').done(function (data){
-    console.log(data);
+
     renderEvents(data);
 })
     .catch(function(error){
@@ -50,13 +48,13 @@ function renderEvent(event, id){
     eventInfo += '<div  id="' + id + '">';
     eventInfo += '<div class="card">';
     eventInfo += '<div class="row g-0">';
-    eventInfo += '<div class="col-md-4">';
+    eventInfo += '<div class="col">';
     eventInfo += '<img src="' + event.image + '" class="img-fluid rounded-start" alt="eventImage"> ';
     eventInfo += '</div>';
     eventInfo += '<div class="col-md-8">';
     eventInfo += '<div class="card-body">'
     eventInfo += '<h2>' + event.title + '</h2>';
-    // eventInfo += '<h6> When: ' + event.date + '</h6>';
+
     eventInfo += '<p> Where: <a href="' + event.location + '" target="_blank"><i class="fa-solid fa-map-location-dot"></i></a></p>';
     eventInfo += '<p>' + event.desc + '</p>';
     eventInfo += '<div id="icons area">';
