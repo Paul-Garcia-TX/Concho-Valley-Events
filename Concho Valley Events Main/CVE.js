30331f58cc235431f8cf53f3c5bf80022a573e51
Paul Garcia <paul.ngarcia@yahoo.com>
2023-05-06 22:09:06 -0500

    converting this to html

--- Concho Valley Events Main/CVE.js
+++ Concho Valley Events Main/CVE.js
@@ -32,58 +32,60 @@ function fetchWeatherData(zipCode){
 
 fetchWeatherData(zipCode);
 
-$.get('events.json').done(function (data){
-
-    renderEvents(data);
-})
-    .catch(function(error){
-        console.error(error);
-    });
-
-function renderEvent(event, id){
-    let eventInfo = "";
-
-
-    // eventInfo += '<div class="col">'
-    // eventInfo += '<div  id="' + id + '">';
-    eventInfo += '<div class="card p-3">';
-    eventInfo += '<div class="row g-0">';
-    eventInfo += '<div class="col-12 col-md">';
-    eventInfo += '<img src="' + event.image + '" class="img-fluid rounded-start" alt="eventImage"> ';
-    eventInfo += '</div>'; // end of image col
-    eventInfo += '<div class="card-body col-8 col-md p-1">';
-    // eventInfo += '<div class="card-body">'
-    eventInfo += '<h2>' + event.title + '</h2>';
-
-    eventInfo += '<p> Where: <a href="' + event.location + '" target="_blank"><i class="fa-solid fa-map-location-dot"></i></a></p>';
-    eventInfo += '<p>' + event.desc + '</p>';
-    eventInfo += '<div id="icons area">';
-    eventInfo += '<a href="'+ event.facebook + '" target="_blank">';
-    eventInfo += '<i class="fa-brands fa-facebook" style="margin: 1em;"></i>';
-    eventInfo += '</a>';
-    eventInfo += '<a href="'+ event.instagram + '" target="_blank">';
-    eventInfo += '<i class="fa-brands fa-instagram" style="margin: 1em;"></i>';
-    eventInfo += '</a>';
-    eventInfo += '<a href="mailto:'+ event.email + '" target="_blank">';
-    eventInfo += '<i class="fa-solid fa-envelope" style="margin: 1em;"></i>';
-    eventInfo += '</a>';
-    eventInfo += '<a href="'+ event.website + '" target="_blank">';
-    eventInfo += '<i class="fa-solid fa-info" style="margin: 1em;"></i>';
-    eventInfo += '</a>';
-    eventInfo += '</div>';
-    eventInfo += '</div>';
-    eventInfo += '</div>';
-    eventInfo += '</div>';
-    // eventInfo += '</div>';
-
-    return eventInfo;
-}
-
-function renderEvents(events){
-    let eventInfo = '';
-    for(let i = events.length -1; i>=0; i--){
-        let id = 'event-' + i;
-        eventInfo += renderEvent(events[i], id);
-    }
-    document.getElementById('cardsRow').innerHTML = eventInfo;
-}
+// $.get('https://raw.githubusercontent.com/Paul-Garcia-TX/Concho-Valley-Events/main/Concho%20Valley%20Events%20Main/events.json').done(function (data){
+//     if (document.getElementById('cardsRow').innerHTML.trim() === '') {
+//         renderEvents(data);
+//     }
+// })
+//     .catch(function(error){
+//         console.error(error);
+//     });
+//
+//
+// function renderEvent(event, id){
+//     let eventInfo = "";
+//
+//
+//     // eventInfo += '<div class="col">'
+//     eventInfo += '<div  id="' + id + '">';
+//     eventInfo += '<div class="card p-3">';
+//     eventInfo += '<div class="row g-0">';
+//     eventInfo += '<div class="col-12 col-md">';
+//     eventInfo += '<img src="' + event.image + '" class="img-fluid rounded-start" alt="eventImage"> ';
+//     eventInfo += '</div>'; // end of image col
+//     eventInfo += '<div class="card-body col-8 col-md p-1">';
+//     // eventInfo += '<div class="card-body">'
+//     eventInfo += '<h2>' + event.title + '</h2>';
+//
+//     eventInfo += '<p> Where: <a href="' + event.location + '" target="_blank"><i class="fa-solid fa-map-location-dot"></i></a></p>';
+//     eventInfo += '<p>' + event.desc + '</p>';
+//     eventInfo += '<div id="icons area">';
+//     eventInfo += '<a href="'+ event.facebook + '" target="_blank">';
+//     eventInfo += '<i class="fa-brands fa-facebook" style="margin: 1em;"></i>';
+//     eventInfo += '</a>';
+//     eventInfo += '<a href="'+ event.instagram + '" target="_blank">';
+//     eventInfo += '<i class="fa-brands fa-instagram" style="margin: 1em;"></i>';
+//     eventInfo += '</a>';
+//     eventInfo += '<a href="mailto:'+ event.email + '" target="_blank">';
+//     eventInfo += '<i class="fa-solid fa-envelope" style="margin: 1em;"></i>';
+//     eventInfo += '</a>';
+//     eventInfo += '<a href="'+ event.website + '" target="_blank">';
+//     eventInfo += '<i class="fa-solid fa-info" style="margin: 1em;"></i>';
+//     eventInfo += '</a>';
+//     eventInfo += '</div>';
+//     eventInfo += '</div>';
+//     eventInfo += '</div>';
+//     eventInfo += '</div>';
+//     eventInfo += '</div>';
+//
+//     return eventInfo;
+// }
+//
+// function renderEvents(events){
+//     let eventInfo = '';
+//     for(let i = events.length -1; i>=0; i--){
+//         let id = 'event-' + i;
+//         eventInfo += renderEvent(events[i], id);
+//     }
+//     document.getElementById('cardsRow').innerHTML = eventInfo;
+// }
